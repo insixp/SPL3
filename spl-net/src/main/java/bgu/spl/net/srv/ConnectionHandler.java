61288e5a69aebe818b680baef64d33797d6ca51b
Tal Gonen <talgonen16@gmail.com>
2021-12-30 19:42:42 +0200

    another modification to start

--- spl-net/src/main/java/bgu/spl/net/srv/ConnectionHandler.java
+++ spl-net/src/main/java/bgu/spl/net/srv/ConnectionHandler.java
@@ -1,10 +1,15 @@
-package bgu.spl.net.srv.bidi;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bgu.spl.net.srv;
 
 import java.io.Closeable;
-import java.io.IOException;
 
-public interface ConnectionHandler<T> extends Closeable{
-
-    void send(T msg) ;
-
+/**
+ * The ConnectionHandler interface for Message of type T
+ */
+public interface ConnectionHandler<T> extends Closeable {
+    
 }
